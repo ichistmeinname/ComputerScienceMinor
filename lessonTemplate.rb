55e0b1e7d3b022e1a1250f51462d0f84aa6aa78e
Sandra Dylus <sad@informatik.uni-kiel.de>
2015-11-13 09:07:22 +0100

    Add template

---
+++ lessonTemplate.rb
@@ -0,0 +1,22 @@
+# Language: Ruby, Level: Level 1
+
+##
+## Übungsbetrieb vom 5.11.2015
+##
+##   alle mit `#` beginnenden Zeilen sind Kommentare
+##
+
+
+#
+## Präsenzaufgabe 1
+#
+
+
+#
+## Präsenzaufgabe 2
+#
+
+
+#
+## Präsenzaufgabe 3
+#
